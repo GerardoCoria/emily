b8f08e5736099102956e449fce7463d8b74f85e7
GC <gcoria1989@gmail.com>
2021-12-30 16:39:24 -0300

    cambios 30-12

--- js/main.js
+++ js/main.js
@@ -1,11 +1,26 @@
 // Botón para abrir y cerrar el menú mobile 
-
 let btnMobile = document.getElementById("btnMobile");
 let menuLista = document.getElementById("barraMobileCaja");
 btnMobile.addEventListener("click", function()
 {
-    menuLista.classList.toggle("menuMobile");
+    menuLista.classList.toggle("menuVisible");
+    menuLista.classList.toggle("ocultarMenu");
 })
+// let menuMobile = document.getElementById("barraMobileCaja")
+// function mostrarMenu (){
+//     btnMobile.addEventListener("click", function()
+//     {
+//         if (menuMobile.style.display="none")
+//         {
+//             menuMobile.style.display="block"
+//         }
+//         else if(menuMobile.style.display="block")
+//         {
+//             menuMobile.style.display="none"
+//         }
+//     })
+// }
+
 
 // Volver arriba con animaciones
 let volverArriba = document.getElementById("volverArriba");
@@ -17,3 +32,8 @@ volverArriba.addEventListener("click", function()
     });
 });
 
+//Cuando la página cargue, se llaman a las funciones
+// document.addEventListener("DOMContentLoaded",()=>
+// {
+//     mostrarMenu();
+// })
